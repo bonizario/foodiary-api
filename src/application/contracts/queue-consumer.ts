df99aebce05c139125821cfdf9717538fa34928b
Gabriel Bonizário <56419828+bonizario@users.noreply.github.com>
2026-08-01 01:07:56 -0300

    feat: add CloudWatch alarm for meals DLQ

--- src/application/contracts/queue-consumer.ts
+++ src/application/contracts/queue-consumer.ts
@@ -1,3 +1,3 @@
-export interface QueueConsumer<TMessage extends Record<string, unknown>> {
+export interface QueueConsumer<TMessage extends Record<string, unknown> = Record<string, unknown>> {
   process(message: TMessage): Promise<void>;
 }
--- src/main/adapters/lambda-sqs-adapter.ts
+++ src/main/adapters/lambda-sqs-adapter.ts
@@ -1,8 +1,7 @@
 import type { QueueConsumer } from "@/application/contracts/queue-consumer";
 import type { SQSEvent, SQSHandler } from "aws-lambda";
 
-// TODO: IMPLEMENT INTERFACE FOR MESSAGE
-export function lambdaSqsAdapter(consumer: QueueConsumer<Record<string, string>>): SQSHandler {
+export function lambdaSqsAdapter(consumer: QueueConsumer): SQSHandler {
   return async (event: SQSEvent): Promise<void> => {
     await Promise.all(
       event.Records.map(async (record) => {
